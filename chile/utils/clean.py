4d1ae88423461eb8ca5d3c3b74e6443a63ad3cdf
Felix-Hz <feluhv@outlook.es>
2023-03-21 17:28:16 -0300

    add chile utils: wrangling | annual_data

--- chile/utils/clean.py
+++ chile/utils/clean.py
@@ -8,43 +8,30 @@ def wrangling(dfs):
     for i in range(len(dfs)):
         df = dfs[i]
 
-        # print('hola')
+        df = df.loc[:, ['Código SACH', 'Fecha',  'Importador', 'Aduana', 'Vía Transporte', 'País de Origen', 'País de Adquisición', 'FOB Unitario U$S', 'FOB U$S', 'Flete U$S', 'Seguro U$S', 'U$S CIF', 'U$S Unitario', 'Cantidad Comercial', 'Unidad de Medida', 'Kgs. Brutos',
+                        'Mercadería', 'Variedad', 'Marca', 'Manifiesto', 'Fecha Manifiesto']]
 
-        df.loc[:, ['NCM-SIM', 'Fecha', 'Tipo de Dato',  'Importador', 'Localidad', 'Destinación', 'Aduana', 'Via Transporte', 'País de Origen',
-                   'País de Procedencia', 'U$S Unitario', 'U$S FOB', 'Flete U$S', 'Seguro U$S', 'U$S CIF', 'Cantidad', 'Unidad de Medida', 'Kgs. Netos',
-                   'Marca - Sufijos', 'Cantidad.1']] = df.loc[:, ['NCM-SIM', 'Fecha', 'Tipo de Dato',  'Importador', 'Localidad', 'Destinación', 'Aduana', 'Via Transporte', 'País de Origen',
-                                                                  'País de Procedencia', 'U$S Unitario', 'U$S FOB', 'Flete U$S', 'Seguro U$S', 'U$S CIF', 'Cantidad', 'Unidad de Medida', 'Kgs. Netos',
-                                                                  'Marca - Sufijos', 'Cantidad.1']]
+        print("~ Limpiando vías de transporte...")
 
         # Se eliminan registros que vengan por aire
-        subset = df['Via Transporte']
-        df = df[subset != 'AVION']
-
-        # Se limpia la palabra MARCA: por redundante
-        df['Marca - Sufijos'] = df['Marca - Sufijos'].str.replace(
-            'MARCA: ', '')
-
-        # Se eliminna registros que no tengan las marcas registradas
-        df.loc[df['Marca - Sufijos'] ==
-               "SIN MARCA", 'Marca - Sufijos'] = "S/M"
-
-        print("~ Limpiando marcas...")
-
-        # Estandarizo los valores del NCM
-        df['NCM-SIM'] = df['NCM-SIM'].str.replace('.', '')
-        df['NCM-SIM'] = df['NCM-SIM'].str.replace('[a-zA-Z]', '')
-        df['NCM-SIM'] = df['NCM-SIM'].str[:6]
+        subset = df['Vía Transporte']
+        df = df[subset != 'AEREA']
 
         print("~ Limpiando NCMs...")
 
-        df["U$S Unitario"] = (
-            df['U$S CIF'] / df['Kgs. Netos']).round(2)
+        # Estandarizo los valores del NCM
+        df['Código SACH'] = df['Código SACH'].astype(str).str.replace('.', '')
+        df['Código SACH'] = df['Código SACH'].astype(str).str.replace('[a-zA-Z]', '')
+        df['Código SACH'] = df['Código SACH'].astype(str).str[:6]
 
         print("~ Creando columna de precio...")
+        df["U$S Unitario"] = (df['U$S CIF'] / df['Cantidad Comercial']).round(2)
 
-        df = df.dropna()
+        # df_new_filtered = df_new.loc[(df_new['U$S Unitario'] <= 1.2)]
 
         print("~ Dropping nulls...")
+
+        df = df.dropna()
 
         df['Fecha'] = pd.to_datetime(df['Fecha'], format='%Y-%m-%d')
 
--- chile/utils/data_anual.py
+++ chile/utils/data_anual.py
@@ -4,29 +4,23 @@ def annual_data(dfs):
     transition_dic = {
         "NCM": [],
         "Año": [],
-        # "Importadores": [],
-        # "Origenes": [],
         "Volumen Total": []
     }
 
     for df in dfs:
-        transition_dic['NCM'].append(''.join(df['NCM-SIM'].unique()))
+        transition_dic['NCM'].append(''.join(df['Código SACH'].unique()))
 
-        transition_dic['Año'].append(df["Fecha"][4].year)
+        transition_dic['Año'].append(df["Fecha"][0].year)
 
-        volumenTotalImportacionTn = (df['Kgs. Netos'].sum()/1000).round(2)
+        volumenTotalImportacionTn = (
+            df['Cantidad Comercial'].sum()/1000).round(2)
 
         transition_dic['Volumen Total'].append(volumenTotalImportacionTn)
 
-        # ACA VA EL RECORTE
-
-        print(f"- {df['Fecha'][4].year} appended.")
+        print(f"- {df['Fecha'][0].year} appended.")
 
     print("~~~~~~~~~~~~~~~~~~~\n> Transition dictionary:")
+
     for key, values in transition_dic.items():
         print(f"- {key}: {values}")
 
-
-    # excel_new_data = excel_new_data.append(transition_df)
-
-    # print(f"> Transition dic: {transition_dic}")

a2607ee84c8c8ae651b39da7ecaa223249456e76
Felix-Hz <feluhv@outlook.es>
2023-03-24 19:21:03 -0300

    add peru

---
+++ peru/utils/clean.py
@@ -0,0 +1,95 @@
+import pandas as pd
+
+
+def wrangling(dfs):
+    '''
+
+        Data wrangling for initial dataframes. This function selects columns, deletes airway products, standardizes NCMs,
+        creates a unitary U$S price, filters unitary price, removes outliers & drops nulls.
+
+            Args:
+                - dfs (list): A list of dataframes to be cleaned and processed.
+
+            Returns:
+                - list: A list of clean dataframes.
+
+    '''
+
+    results_dfs = []
+
+    print(f"\n> MUNGING HISTORICAL DATA:\n")
+
+    for i in range(len(dfs)):
+        df = dfs[i]
+        year_error_memory = df["Fecha"].iloc[0].year
+
+        df = df.loc[:, ['Código NCM', 'Fecha', 'País de Origen', 'Importador', 'Cantidad Comercial', 'Unidad de Medida',  'Tipo de Bulto',
+                        'Proveedor', 'Aduana', 'Vía Transporte', 'U$S CIF', 'U$S FOB', 'Kgs. Netos', 'Marca', 'Descripción de Mercadería', 'Descripción para Filtro']]
+
+        # Se eliminan registros que vengan por aire
+        subset = df['Vía Transporte']
+        df = df[subset != 'AEREA']
+
+        df.loc[df['Marca'] ==
+               "SIN MARCA", 'Marca'] = "S/M"
+
+        df.loc[df['Marca'] ==
+               "SIN  MARCA", 'Marca'] = "S/M"
+        df.loc[df['Marca'] ==
+               "SM", 'Marca'] = "S/M"
+        df.loc[df['Marca'] ==
+               "N/M", 'Marca'] = "S/M"
+        df.loc[df['Marca'] ==
+               "SMARCA", 'Marca'] = "S/M"
+        df.loc[df['Marca'] ==
+               "S/N", 'Marca'] = "S/M"
+        df.loc[df['Marca'] ==
+               "SN", 'Marca'] = "S/M"
+        df.loc[df['Marca'] ==
+               "No disponible", 'Marca'] = "S/M"
+
+        print("~ Limpiando NCMs...")
+
+        # Estandarizo los valores del NCM
+
+        df['Código NCM'] = df.loc[:, 'Código NCM'] = 283525
+
+        df["U$S Unitario"] = (
+            df['U$S CIF'] / df['Kgs. Netos']).round(2)
+
+        print("~ Creando columna de precio...")
+
+        df = df.loc[df['U$S Unitario'] <= 1.2]
+
+        print("~ Dropping outliers...")
+
+        q1 = df['U$S Unitario'].quantile(0.25)
+        q3 = df['U$S Unitario'].quantile(0.75)
+
+        interquartileRange = q3 - q1
+        # use outlier step (1.5) to determine the boundaries w/ iqr to filter the price with
+        lower_bound = q1 - 1.5 * interquartileRange
+        upper_bound = q3 + 1.5 * interquartileRange
+
+        # drop outliers
+        outlier_indices = df[(df['U$S Unitario'] < lower_bound) | (
+            df['U$S Unitario'] > upper_bound)].index
+        df = df.drop(outlier_indices)
+
+        print("~ Dropping nulls...")
+
+        df = df.dropna()
+
+        df['Fecha'] = pd.to_datetime(df['Fecha'], format='%Y-%m-%d')
+
+        results_dfs.append(df)
+
+        dfs[i] = df
+
+        if df is not None and not df.empty and not pd.isnull(df["Fecha"].iloc[0].year):
+            print(
+                f'> Done with: {df["Fecha"].iloc[0].year}\n~~~~~~~~~~~~~~~~~~~')
+        else:
+            print(f'> No data for: {year_error_memory}\n~~~~~~~~~~~~~~~~~~~')
+
+    return results_dfs
---
+++ peru/utils/vol_origenes.py
@@ -0,0 +1,60 @@
+import pandas as pd
+
+
+def analisis_origenes(dfs):
+    '''
+
+        Analyses the origin of imported goods over multiple years for given datasets.
+
+            Parameters:
+                - dfs (list): A list of pandas DataFrames containing data on imported goods.
+
+            Returns:
+                - DataFrame: Information on the origins of imported goods over multiple years.
+
+    '''
+
+    excel_new_data = pd.DataFrame()
+
+    for df in dfs:
+
+        volumenTotalImportacionTn = (df['Kgs. Netos'].sum()/1000).round(2)
+
+        registro_volumen = {
+            "NCM": [],
+            "Año": [],
+            "Pais": [],
+            "No. Importaciones": [],
+            "Volumen Total (TN)": [],
+            "Participacion en Vol. Total": []
+        }
+
+        for pais in df['País de Origen'].unique():
+
+            data = df[df['País de Origen'] == f"{pais}"]
+
+            volumenTotalOrigen = (data['Kgs. Netos'].sum()/1000).round(2)
+
+            if (data['Kgs. Netos'].sum() > 5000):
+
+                registro_volumen['NCM'].append(''.join(str(df['Código NCM'].unique().tolist())[1:-1]))
+                registro_volumen['Año'].append(
+                    str(data["Fecha"].iloc[0].year)[:4])
+                registro_volumen['Pais'].append(pais)
+                registro_volumen['No. Importaciones'].append(len(data))
+                registro_volumen['Volumen Total (TN)'].append(
+                    (data['Kgs. Netos'].sum()/1000).round(2))
+                registro_volumen['Participacion en Vol. Total'].append(
+                    f"{round(( volumenTotalOrigen / volumenTotalImportacionTn) *100)}%")
+                print(f"- Done with: {pais} ({df['Fecha'].iloc[0].year}) ")
+
+        transition_df = pd.DataFrame.from_records(registro_volumen).sort_values(
+            'Volumen Total (TN)', ascending=False).reset_index(drop=True)
+        transition_df = transition_df[[
+            'NCM', 'Año', 'Pais', 'Volumen Total (TN)', "Participacion en Vol. Total", 'No. Importaciones']]
+        excel_new_data = excel_new_data.append(
+            transition_df, ignore_index=False)
+
+    print("~~~~~~~~~~~~~~~~~~~\n> Current dataframe of the origins of each year:")
+
+    return excel_new_data
